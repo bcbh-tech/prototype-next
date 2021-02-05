a3970798d43d9432b672e87bd6b7d2b7fd07ef29
Josh Lohse <yoshwaaa@gmail.com>
2021-02-04 21:46:17 -0500

    First commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,9 @@
-import '../styles/globals.css'
+// import '../styles/globals.css'
+import '../styles/main.css'
+import '../styles/app.css'
+import '../styles/header.css'
+import '../styles/footer.css'
+
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
