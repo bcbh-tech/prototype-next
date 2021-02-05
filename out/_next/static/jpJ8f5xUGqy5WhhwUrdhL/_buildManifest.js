a1c0679dca3a3db921316fbc285675f5a6289280
Josh Lohse <yoshwaaa@gmail.com>
2021-02-05 14:02:45 -0500

    Second deployment test

--- out/_next/static/jpJ8f5xUGqy5WhhwUrdhL/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-7858ea6a2c9384280ec6.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-594289777a4a66e3a2c9.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
